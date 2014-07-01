7e89230f62795bdb11ad6d6b47b601fa3dee3337
phitchcock <phitchcock@me.com>
2014-06-30 21:38:42 -0700

    created initial category resource including migration, model, controller and views

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,58 @@
+class CategoriesController < ApplicationController
+
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to root_path, notice: "#{@category.title} was created"
+    else
+      flash[:error] = "Catory was not saved, please try again"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to category_path(@category), notice: "#{@category.title} was updated"
+    else
+      flash[:error] = "Category was not updated, please try again"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      redirect_to root_path, notice: "#{@category.title} was destroyed"
+    else
+      flash[:error] = "Category was not destroyed please try again"
+      redirect_to category_path(@category)
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit!
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+end
